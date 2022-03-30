5d7abd83a0da2494b471c9a88916b800408b278c
Mathew de Vin <mathewdevin03@gmail.com>
2022-03-30 12:49:51 -0700

    Update sidebar title and styling
     - Update app title
     - Add circle to represent a logo
     - Add bg to active link

--- src/components/SidebarNav/SidebarNav.tsx
+++ src/components/SidebarNav/SidebarNav.tsx
@@ -6,7 +6,7 @@ const SidebarNav = () => {
   const CustomNavLink = useCallback((route: string, text: string): ReactElement => {
     const navLinkStyles = ({isActive}: { isActive: boolean }): string => {
       const defaultStyle = 'flex py-1 px-2 hover:bg-gray-100 hover:text-black hover:font-semibold rounded';
-      return `${defaultStyle} ${isActive ? 'text-black font-semibold' : 'text-gray-500'}`;
+      return `${defaultStyle} ${isActive ? 'text-black font-semibold bg-gray-100' : 'text-gray-500'}`;
     };
     
     return (
@@ -18,7 +18,10 @@ const SidebarNav = () => {
   
   return (
     <aside className={'w-64 h-screen border-r border-r-gray-100 p-3 sticky top-0'}>
-      <span className={'text-2xl font-bold'}>App</span>
+      <div className={'flex items-center'}>
+        <div className={'w-8 h-8 rounded-full bg-gray-400 mr-3'}/>
+        <span className={'text-2xl font-bold'}>Team Viz</span>
+      </div>
       <nav className={'mt-5'}>
         <ul className={'flex flex-col gap-2'}>
           <li>
