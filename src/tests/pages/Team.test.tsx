e9e819ec8b9cafb1190568da6c36f724d494cf6b
Mathew de Vin <mathewdevin03@gmail.com>
2022-03-29 22:06:23 -0700

    Fix test

--- src/tests/pages/Team.test.tsx
+++ src/tests/pages/Team.test.tsx
@@ -3,7 +3,7 @@ import {Team} from '../../pages';
 import {screen, waitForElementToBeRemoved} from '@testing-library/react';
 import {Route, Routes} from 'react-router-dom';
 import {routes} from '../../routing';
-import {getAssignments, getTeamStudents} from '../../api/mocks/utils';
+import {getTeamStudents} from '../../api/mocks/utils';
 import {mockTeams} from '../../api/mocks';
 
 describe('Team Page', () => {
@@ -28,14 +28,6 @@ describe('Team Page', () => {
     const students = getTeamStudents(mockTeam.teamSetId, mockTeam.id);
     students.forEach(student => {
       expect(screen.getByText(student.name)).toBeInTheDocument();
-    });
-  });
-  
-  it('should display assignments section', () => {
-    expect(screen.getByText(/Assignments/i)).toBeInTheDocument();
-    const assignments = getAssignments(true);
-    assignments.forEach(assignment => {
-      expect(screen.getByText(assignment.name)).toBeInTheDocument();
     });
   });
 });
