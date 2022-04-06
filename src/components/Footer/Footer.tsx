70f5747792e946bf274670d49a23726ef80c8074
Mathew de Vin <mathewdevin03@gmail.com>
2022-04-06 10:48:32 -0700

    Update footer details

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,11 @@
 const Footer = () => {
   return (
     <footer className={'w-full border-t border-t-gray-100 p-3'}>
-      Footer Details
+      <div className={'ml-auto w-fit text-sm'}>
+        <span className={'font-semibold'}>Team Viz</span>
+        &nbsp;by&nbsp;
+        <span className={'font-semibold'}>Mathew de Vin</span>
+      </div>
     </footer>
   );
 };
